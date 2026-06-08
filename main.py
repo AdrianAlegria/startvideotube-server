ba1554f468c5de4de6220e1874107418afafd3b3
AdrianAlegria <133515943+AdrianAlegria@users.noreply.github.com>
2026-06-07 20:10:18 -0600

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("startvideotube")
 
 # ─── App ───────────────────────────────────────────────────────────────────────
-app = FastAPI(title="StartVideoTube", version="5.3.1")
+app = FastAPI(title="StartVideoTube", version="5.3.2")
 
 # ─── Variables de entorno ──────────────────────────────────────────────────────
 FISH_API_KEY  = os.environ.get("FISH_API_KEY", "")
